c2c5857e7bc1f1bb6ed057ec4939d6c79531f616
Deepjyot Singh Kapoor <kapoordeepjyotsingh29@gmail.com>
2025-09-05 09:51:10 -0700

    Update dependencies in pyproject.toml and uv.lock; add new project for experimenting with temporal workflows. Enhance multiprocessing example with improved print statements for clarity.

--- multiprocessing/01_multiprocessing.py
+++ multiprocessing/01_multiprocessing.py
@@ -9,28 +9,34 @@ The solution is to use multiple processes instead of multiple threads.
 
 Are python threads bind to platform threads? Yes, they are. But, the GIL is a global lock that prevents
 multiple threads from executing Python code simultaneously.
+
+
+So, alll in all python is a single thread language, but we can use multiprocessing to run multiple processes in parallel.
 """
 import os
 from multiprocessing import Process
 import time
 
-# print(f"Module level - __name__: {__name__}, PID: {os.getpid()}")
+print(f"Module level - __name__: {__name__}, PID: {os.getpid()}")
 
 def cpu_bound():
-    # print(f"Child process - __name__: {__name__}, PID: {os.getpid()}")
+    print(f"Child process - __name__: {__name__}, PID: {os.getpid()}")
     total = 0
     for i in range(10**7):
         total += i
 
 if __name__ == '__main__':
-    # print(f"Main process - __name__: {__name__}, PID: {os.getpid()}")
+    print(f"Main process - __name__: {__name__}, PID: {os.getpid()}")
     start = time.time()
     processes = []
     for i in range(4):
         p = Process(target=cpu_bound)
         processes.append(p)
+        # i am starting the process
         p.start()
 
+    
+    # Wait for all processes to finish
     for p in processes:
         p.join()
     
